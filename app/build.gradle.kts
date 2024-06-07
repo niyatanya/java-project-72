58c8677fb4a3051d8682b1ae87af454038b92f43
Naumenko Tatiana <jouk@list.ru>
2024-06-07 09:24:43 +0300

    Add Dockerfile, uadd Javalin and logger to App.java and build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.javalin:javalin:6.1.3")
+    implementation("org.slf4j:slf4j-simple:2.0.13")
+    implementation("io.javalin:javalin-rendering:6.1.3")
 }
 
 tasks.test {
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,17 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var app = getApp();
+        app.start(7070);
+    }
+
+    public static Javalin getApp(){
+        Javalin app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+        app.get("/", ctx -> ctx.result("Hello World"));
+        return app;
     }
 }
